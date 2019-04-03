5af43795ddd6be8c37366303484fa14843bc295b
Eric Stocklassa <eric.stocklassa@mayflower.de>
2019-04-03 09:02:28 +0200

    Newest changes

--- vim-compile.sh
+++ vim-compile.sh
@@ -2,10 +2,10 @@ cd ~/vim
 ./configure --with-features=huge \
 --enable-multibyte \
 --enable-rubyinterp=yes \
---enable-pythoninterp=dynamic \
+--enable-pythoninterp=yes \
 --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
---enable-python3interp \
---with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu \
+# --enable-python3interp \
+# --with-python3-config-dir=/usr/lib/python3.6/config-3.6m-x86_64-linux-gnu \
 --enable-perlinterp=yes \
 --enable-luainterp=yes \
 --enable-gui=gtk2 \
